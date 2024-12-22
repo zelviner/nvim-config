7908251918867a63b8868d0b5e52da39f5caf916
zelvin <1362848545@qq.com>
2024-12-22 23:21:33 +0800

    add neovim config

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,19 @@
+-- 主键
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- ---------- 插入模式 ---------- --
+
+-- ---------- 观看模式 ---------- --
+keymap.set("v","J",":m '>+1<CR>gv=gv")
+keymap.set("v","K",":m '<-2<CR>gv=gv")
+
+
+-- ---------- 正常模式 ---------- --
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口
+keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+
+-- 取消高亮
+keymap.set("n", "<leader>nh",":nohl<CR>")
